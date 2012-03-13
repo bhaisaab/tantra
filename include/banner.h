0cfaab8e6a6b601aeaeca083576ca160b91706ff
Rohit Yadav <rohityadav89@gmail.com>
2012-03-13 05:38:23 +0530

    banner: project banner

---
+++ include/banner.h
@@ -0,0 +1,7 @@
+"\n               I'm a monolithic unix based"
+"\n \\_\\_    _/_/  kernel created by"
+"\n     \\__/      Rohit Yadav"
+"\n     (o0)\\________"
+"\n     (#_)\\ TANTRA )\\"
+"\n        ||------w |"
+"\n        ||       ||\n"
