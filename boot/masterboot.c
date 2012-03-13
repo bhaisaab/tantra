5424eca7ec8e958debda04c9ffe88829325bb7cb
Rohit Yadav <rohityadav89@gmail.com>
2012-03-14 03:47:39 +0530

    Boot: Fix gdt, masterboot with simple logic Code forked from xv6

--- boot/masterboot.c
+++ boot/masterboot.c
@@ -1,96 +1,90 @@
-#include <x86.h>
+#include <types.h>
 #include <elf.h>
+#include <x86.h>
 
 #define SECTSIZE  512
 #define PAGESIZE  4096
-#define ELFHDR    ((struct Elf *) 0x10000) // scratch space
 
-void read_sector(void *addr, uint32_t sectornum);
-void read_segment(uint32_t va, uint32_t filesz, uint32_t memsz, uint32_t offset);
+void read_segment(uint8_t*, uint32_t, uint32_t);
 
 void
 masterboot(void)
 {
-    struct Proghdr *ph, *eph;
+  struct elfhdr *elf;
+  struct proghdr *ph, *eph;
+  void (*entry)(void);
+  uint8_t* pa;
 
-    // read 1st page off disk
-    read_segment((uint32_t) ELFHDR, PAGESIZE, PAGESIZE, 0);
+  elf = (struct elfhdr*) 0x10000; // scratch space
 
-    // is this a valid ELF?
-    if (ELFHDR->e_magic != ELF_MAGIC)
-        goto bad;
+  // Read first page off disk
+  read_segment((uint8_t*) elf, PAGESIZE, 0);
 
-    // load each program segment (ignores ph flags)
-    ph = (struct Proghdr *) ((uint8_t *) ELFHDR + ELFHDR->e_phoff);
-    eph = ph + ELFHDR->e_phnum;
-    for (; ph < eph; ph++)
-        read_segment(ph->p_va, ph->p_filesz, ph->p_memsz, ph->p_offset);
+  // Is this a valid ELF?
+  if (elf->magic != ELF_MAGIC)
+    return;
 
-    // call the entry point from the ELF header
-    // note: does not return!
-    ((void (*)(void)) (ELFHDR->e_entry & 0xFFFFFF))();
-bad:
-    outw(0x8A00, 0x8A00);
-    outw(0x8A00, 0x8E00);
-    /* boot.S will spin for us */
-}
+  // Load each program segment (ignores ph flags)
+  ph = (struct proghdr *) ((uint8_t*) elf + elf->phoff);
+  eph = ph + elf->phnum;
+  for (; ph < eph; ph++) {
+    pa = (uint8_t*) ph->paddr;
+    read_segment(pa, ph->filesz, ph->off);
+    if (ph->memsz > ph->filesz)
+      stosb(pa + ph->filesz, 0, ph->memsz - ph->filesz);
+  }
 
-// Read 'filesz' bytes at 'offset' from kernel into virtual address 'va',
-// then clear the memory from 'va+filesz' up to 'va+memsz' (set it to 0).
-void
-read_segment(uint32_t va, uint32_t filesz, uint32_t memsz, uint32_t offset)
-{
-    uint32_t end_va;
-
-    va &= 0xFFFFFF;
-    end_va = va + filesz;
-    memsz += va;
-
-    // round down to sector boundary
-    va &= ~(SECTSIZE - 1);
-
-    // translate from bytes to sectors, and kernel starts at sector 1
-    offset = (offset / SECTSIZE) + 1;
-
-    // If this is too slow, we could read lots of sectors at a time.
-    // We'd write more to memory than asked, but it doesn't matter --
-    // we load in increasing order.
-    while (va < end_va) {
-        read_sector((uint8_t*) va, offset);
-        va += SECTSIZE;
-        offset++;
-    }
-
-    // clear bss segment
-    while (end_va < memsz)
-        *((uint8_t*) end_va++) = 0;
+  // Call the entry point from the ELF header
+  entry = (void(*)(void))(elf->entry);
+  entry();
 }
 
 void
 waitdisk(void)
 {
-    // wait for disk reaady
-    while ((inb(0x1F7) & 0xC0) != 0x40);
+  // wait for disk reaady
+  while ((inb(0x1F7) & 0xC0) != 0x40);
 }
 
+// Read a single sector at offset into dst
 void
 read_sector(void *dst, uint32_t offset)
 {
-    // wait for disk to be ready
-    waitdisk();
+  // wait for disk to be ready
+  waitdisk();
 
-    // Scan IDE controller for hard disk
-    outb(0x1F2, 1); // count = 1
-    outb(0x1F3, offset);
-    outb(0x1F4, offset >> 8);
-    outb(0x1F5, offset >> 16);
-    outb(0x1F6, (offset >> 24) | 0xE0);
-    outb(0x1F7, 0x20); // cmd 0x20 - read sectors
+  // Scan IDE controller for hard disk
+  outb(0x1F2, 1); // count = 1
+  outb(0x1F3, offset);
+  outb(0x1F4, offset >> 8);
+  outb(0x1F5, offset >> 16);
+  outb(0x1F6, (offset >> 24) | 0xE0);
+  outb(0x1F7, 0x20); // cmd 0x20 - read sectors
 
-    // wait for disk to be ready
-    waitdisk();
+  // wait for disk to be ready
+  waitdisk();
 
-    // read a sector
-    insl(0x1F0, dst, SECTSIZE/4);
+  // read a sector
+  insl(0x1F0, dst, SECTSIZE/4);
 }
 
+// Read 'count' bytes at 'offset' from kernel into physical address 'pa'.
+// Might copy more than asked.
+void
+read_segment(uint8_t* pa, uint32_t count, uint32_t offset)
+{
+  uint8_t* epa;
+  epa = pa + count;
+
+  // round down to sector boundary
+  pa -= offset % SECTSIZE;
+
+  // translate from bytes to sectors, and kernel starts at sector 1
+  offset = (offset / SECTSIZE) + 1;
+
+  // If this is too slow, we could read lots of sectors at a time.
+  // We'd write more to memory than asked, but it doesn't matter --
+  // we load in increasing order.
+  for (; pa < epa; pa += SECTSIZE, offset++)
+    read_sector(pa, offset);
+}
