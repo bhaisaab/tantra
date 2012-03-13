060fc21b71567cd0b378893f71a0e70bbce2d3e2
Rohit Yadav <rohityadav89@gmail.com>
2012-03-13 05:38:01 +0530

    include: headers

---
+++ include/types.h
@@ -0,0 +1,34 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+typedef char                int8_t;
+typedef short               int16_t;
+typedef int                 int32_t;
+typedef long long           int64_t;
+
+typedef unsigned char       uint8_t;
+typedef unsigned short      uint16_t;
+typedef unsigned int        uint32_t;
+typedef unsigned long long  uint64_t;
+
+typedef uint32_t            size_t;
+
+typedef int32_t             intptr_t;
+typedef uint32_t            uintptr_t;
+typedef uint32_t            physaddr_t;
+
+#define MIN(_a, _b) \
+({  \
+  typeof(_a) __a = (_a);  \
+  typeof(_b) __b = (_b);  \
+  __a <= __b ? __a : __b; \
+})
+
+#define MAX(_a, _b) \
+({  \
+  typeof(_a) __a = (_a);  \
+  typeof(_b) __b = (_b);  \
+  __a >= __b ? __a : __b; \
+})
+
+#endif
---
+++ include/x86.h
@@ -0,0 +1,40 @@
+#ifndef X86_H
+#define X86_H
+
+#include <types.h>
+
+static __inline uint8_t inb(int port) __attribute__((always_inline));
+static __inline void insl(int port, void *addr, int cnt) __attribute__((always_inline));
+static __inline void outb(int port, uint8_t data) __attribute__((always_inline));
+static __inline void outw(int port, uint16_t data) __attribute__((always_inline));
+
+static __inline uint8_t
+inb(int port)
+{
+  uint8_t data;
+  __asm __volatile("inb %w1,%0" : "=a" (data) : "d" (port));
+  return data;
+}
+
+static __inline void
+insl(int port, void *addr, int cnt)
+{
+  __asm __volatile("cld\n\trepne\n\tinsl" :
+    "=D" (addr), "=c" (cnt) :
+    "d" (port), "0" (addr), "1" (cnt) :
+    "memory", "cc");
+}
+
+static __inline void
+outb(int port, uint8_t data)
+{
+  __asm __volatile("outb %0,%w1" : : "a" (data), "d" (port));
+}
+
+static __inline void
+outw(int port, uint16_t data)
+{
+  __asm __volatile("outw %0,%w1" : : "a" (data), "d" (port));
+}
+
+#endif
