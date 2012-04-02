32c400b107d6e226ff0bd8eed097d4da2cf21f85
Rohit Yadav <rohityadav89@gmail.com>
2012-04-02 17:39:13 +0530

    lib: stdio naive printf implementation

--- lib/stdio.c
+++ lib/stdio.c
@@ -1,2 +1,16 @@
 #include <stdio.h>
 
+extern void putchar(char, int);
+
+void
+printf(const char *format, ...)
+{
+    char **arg = (char **) &format;
+    int c;
+    arg++;
+    while ( (c = *format++) != 0 )
+    {
+        if (c != '%')
+            putchar(c, 2);
+    }
+}
