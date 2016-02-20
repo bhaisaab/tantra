0ea70b0a3ab71741104a48c00272f4c1213c37c1
Rohit Yadav <bhaisaab@baagi.org>
2016-02-20 19:24:06 +0100

    libc: implement string.h methods
    Signed-off-by: Rohit Yadav <bhaisaab@baagi.org>

--- src/include/libc/string.h
+++ src/include/libc/string.h
@@ -2,6 +2,16 @@
 #define STRING_H
 
 size_t strlen(const char *str);
-void memset(void *str, char c, size_t n);
+
+int strcmp(const char *p, const char *q);
+int strncmp(const char *p, const char *q, size_t n);
+
+char* strcpy(char *dst, const char *src);
+char* strncpy(char *dst, const char *src, size_t size);
+char* strchr(const char *s, char c);
+
+void memset(void* ptr, char chr, uint32_t size);
+int memcmp(const void *v1, const void *v2, size_t n);
+void* memcpy(void *dst, void *src, size_t n);
 
 #endif
--- src/libc/string.c
+++ src/libc/string.c
@@ -8,10 +8,71 @@ size_t strlen(const char *str)
     return size;
 }
 
+int strcmp(const char *p, const char *q)
+{
+    while (*p && *p == *q) ++p, ++q;
+    return (int) ((unsigned char) *p - (unsigned char) *q);
+}
+
+int strncmp(const char *p, const char *q, size_t n)
+{
+    while (n > 0 && *p && *p == *q) --n, ++p, ++q;
+    if (n == 0) return 0;
+    else return (int) ((unsigned char) *p - (unsigned char) *q);
+}
+
+char* strcpy(char *dst, const char *src)
+{
+    char *ret = dst;
+    while ((*dst++ = *src++) != '\0');
+    return ret;
+}
+
+char* strncpy(char *dst, const char *src, size_t size)
+{
+    char *ret = dst;
+    for (size_t i = 0; i < size; ++i) {
+        *dst++ = *src;
+        if (*src != '\0') src++;
+    }
+    return ret;
+}
+
+char* strchr(const char *s, char c)
+{
+    for (; *s; ++s) {
+        if (*s == c) return (char *) s;
+    }
+    return (char *) 0;
+}
+
 void memset(void* ptr, char chr, uint32_t size)
 {
     uint8_t *p = (uint8_t*) ptr;
     while (size > 0 && size--) {
         *p++ = chr;
     }
+}
+
+void* memcpy(void *dst, void *src, size_t n)
+{
+    const uint8_t *s = src;
+    uint8_t *d = dst;
+    while (n-- > 0) {
+        *d++ = *s++;
+    }
+    return dst;
+}
+
+int memcmp(const void *v1, const void *v2, size_t n)
+{
+  const uint8_t *s1 = (const uint8_t *) v1;
+  const uint8_t *s2 = (const uint8_t *) v2;
+
+  while (n-- > 0) {
+    if (*s1 != *s2) return (int) *s1 - (int) *s2;
+    s1++, s2++;
+  }
+
+  return 0;
 }
